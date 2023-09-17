ba51ffc1457fc98903a653c70782c60fe1d7d8f3
sarathrajan922 <sarathrajan922@gmail.com>
2023-09-17 23:20:04 +0530

    server setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("superb....")
+import express from 'express';
+import http from 'http';
+import { Server  } from 'socket.io';
+
+const port = 8080;
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server); 
+io.on('connection', ()=>{
+    console.log('user is connected');
+});
+server.listen(port,()=>{
+    console.log(`server listening on port ${port}`)
+});
