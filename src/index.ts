feb318a7c813f61b048296ce487bba87a6e8a00f
sarathrajan922 <sarathrajan922@gmail.com>
2023-10-09 21:03:26 +0530

    logo removed

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,24 @@
 import express from 'express';
 import http from 'http';
 import { Server  } from 'socket.io';
+import cors from 'cors'
 
 const port = 8080;
 const app = express();
+app.use(cors)
 const server = http.createServer(app);
-const io = new Server(server); 
-io.on('connection', ()=>{
+const io = new Server(server, {
+    cors:{
+        origin: true,
+        methods: ["GET", "POST"]
+    }
+});
+io.on('connection', (socket)=>{
     console.log('user is connected');
+
+    socket.on('disconnect',()=>{
+        console.log("user is disconnected")
+    })
 });
 server.listen(port,()=>{
     console.log(`server listening on port ${port}`)
