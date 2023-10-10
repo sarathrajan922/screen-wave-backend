c44dbc63323516f07f155dd2d49dd259b41796d8
sarathrajan922 <sarathrajan922@gmail.com>
2023-10-10 22:44:27 +0530

    stream setup done

--- src/room/index.ts
+++ src/room/index.ts
@@ -1,19 +1,44 @@
 import { Socket } from "socket.io";
 import { v4 as uuidV4 } from "uuid";
 
+const rooms: Record<string, string[]> = {};
+
+interface IRoomParams {
+  roomId: string;
+  peerId: string;
+}
+
 const roomHandler = (socket: Socket) => {
   const createRoom = () => {
     const roomId = uuidV4();
-
+    rooms[roomId] = [];
     socket.emit("room-created", { roomId });
     console.log("create room success..");
   };
 
-  const joinRoom = ({ roomId }: { roomId: string }) => {
-    console.log(`join room success..
-        Id : ${roomId}`);
-    socket.join(roomId);
+  const joinRoom = ({ roomId, peerId }: IRoomParams) => {
+    if (rooms[roomId]) {
+      console.log(`join room success..
+      roomId : ${roomId}`);
+      console.log(`    peerId: ${peerId}`);
+      rooms[roomId].push(peerId);
+      socket.join(roomId);
+      socket.to(roomId).emit('user-joined', {peerId})
+      socket.emit("get-users", {
+        roomId,
+        participants: rooms[roomId],
+      });
+    }
+    socket.on('disconnect', ()=>{
+      console.log('user left the room ', roomId)
+      leaveRoom({roomId,peerId})
+    })
   };
+
+  const leaveRoom = ({roomId, peerId}: IRoomParams)=>{
+    rooms[roomId] = rooms[roomId]?.filter((id)=> id !== peerId);
+    socket.to(roomId).emit('user-disconnected', peerId)
+  }
 
   socket.on("join-room", joinRoom);
   socket.on("create-room", createRoom);
